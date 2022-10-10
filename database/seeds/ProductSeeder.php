359ebb0a8abfa6fcf34c09cbb4bae68cb72c8dbb
markcaf <marcocafassog@gmail.com>
2022-10-10 12:44:49 +0200

    feat: add food items

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -14,27 +14,27 @@ class ProductSeeder extends Seeder
     {
         $products = [
             [
-                'name' => 'Samsung Galaxy',
-                'description' => 'Samsung Brand',
-                'image' => 'https://dummyimage.com/200x300/000/fff&text=Samsung',
+                'name' => 'Burger Galaxy',
+                'description' => 'Hamburger',
+                'image' => 'https://assets.bonappetit.com/photos/5b919cb83d923e31d08fed17/1:1/w_2560%2Cc_limit/basically-burger-1.jpg',
                 'price' => 100
             ],
             [
-                'name' => 'Apple iPhone 12',
-                'description' => 'Apple Brand',
-                'image' => 'https://dummyimage.com/200x300/000/fff&text=Iphone',
+                'name' => 'Wakame',
+                'description' => 'Sushi',
+                'image' => 'https://i0.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2020/06/4362_Goma.jpg?resize=895%2C616&ssl=1',
                 'price' => 500
             ],
             [
-                'name' => 'Google Pixel 2 XL',
-                'description' => 'Google Pixel Brand',
-                'image' => 'https://dummyimage.com/200x300/000/fff&text=Google',
+                'name' => 'Margherita',
+                'description' => 'Pizza',
+                'image' => 'https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/styles/recipes_1200_800_fallback/public/fdl_content_import_it/margherita-50kalo.jpg?itok=v9nHxNMS',
                 'price' => 400
             ],
             [
-                'name' => 'LG V10 H800',
-                'description' => 'LG Brand',
-                'image' => 'https://dummyimage.com/200x300/000/fff&text=LG',
+                'name' => 'Taiwanese',
+                'description' => 'Taiwan',
+                'image' => 'https://www.agrodolce.it/app/uploads/2019/03/shutterstock-774713827.jpg',
                 'price' => 200
             ]
         ];
