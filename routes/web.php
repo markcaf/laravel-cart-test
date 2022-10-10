42766ee949c028ea4a1ed289ab7b1fa2d634791a
markcaf <marcocafassog@gmail.com>
2022-10-10 16:29:15 +0200

    feat: add checkout route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('cart', [ProductController::class, 'cart'])->name('cart');
 Route::get('add-to-cart/{id}', [ProductController::class, 'addToCart'])->name('add.to.cart');
 Route::patch('update-cart', [ProductController::class, 'update'])->name('update.cart');
 Route::delete('remove-from-cart', [ProductController::class, 'remove'])->name('remove.from.cart');
+Route::get('checkout', [ProductController::class, 'checkout'])->name('checkout');
